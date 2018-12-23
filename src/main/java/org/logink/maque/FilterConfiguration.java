f17e6825a9d50a243a43504553a73ad25a790c89
jidan <jidanhuawei@163.com>
2018-12-23 22:49:22 +0800

    remove eagle filter

--- src/main/java/org/logink/maque/FilterConfiguration.java
+++ src/main/java/org/logink/maque/FilterConfiguration.java
@@ -1,6 +1,5 @@
 package org.logink.maque;
 
-import com.alibaba.arms.filter.EagleEyeFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +13,11 @@ public class FilterConfiguration {
     public FilterRegistrationBean filterDemo4Registration() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
         //注入过滤器
-        registration.setFilter(new EagleEyeFilter());
+        registration.setFilter(new DemoFilter());
         //拦截规则
         registration.addUrlPatterns("/*");
         //过滤器名称
-        registration.setName("EagleEyeFilter");
+        registration.setName("DemoFilter");
         //是否自动注册 false 取消Filter的自动注册
         registration.setEnabled(true);
         //过滤器顺序
